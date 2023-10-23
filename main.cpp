a18bae8f8ec966375da548cf2c53d273075eb146
Iridakon <71597136+Iridakon@users.noreply.github.com>
2023-10-23 21:42:51 +0700

    разность

--- main.cpp
+++ main.cpp
@@ -56,8 +56,28 @@
          std::cout<<')';
      }
  };
+ class Sub :public Expression
+ {
+     Expression *e1,*e2;
+ public:
+     Sub( Expression *slag1,Expression *slag2){
+         e1=slag1;
+         e2=slag2;
+     }
+     Expression* diff() override
+     {
+         return new Sub(e1->diff(),e2->diff());
+     }
+     void print() override {
+         std::cout<<'(';
+         e1->print();
+         std::cout<<'-';
+         e2->print();
+         std::cout<<')';
+     }
+ };
 int main() {
-    Expression *e = new Add(new Number(1), new Variable('x'));
+    Expression *e = new Sub(new Number(1), new Variable('x'));
     e->print();
     std::cout << "\n";
     Expression *de = e->diff();
